42af8eb4356a0d4e1f93255aa90969995f44761f
Alejandrogf <alejandrogarciaf@gmail.com>
2015-10-07 14:28:39 +0200

    Practica Final Modulo 1 v1.1

---
+++ Práctica Módulo1/motorIndex.js
@@ -0,0 +1,35 @@
+﻿
+document.getElementById("btnAccederIndex").addEventListener("click", login);
+
+if (localStorage.getItem("nombreIndex")) {
+    location.replace("registroClase.html");
+}
+
+function login() {
+    if (document.getElementById("txtNombreIndex").value == "") {
+        alert("Nombre vacio");
+        $("#txtNombreIndex").val("");
+        return;
+    }
+    localStorage.setItem("nombreIndex", $("#txtNombreIndex").val());
+    location.replace("registroClase.html");
+
+}
+
+$(document).ready(function () {
+
+    greet();
+
+});
+
+
+
+function greet() {
+    var name = localStorage.getItem("nombreIndex");
+    if (name == null || name == "null") {
+        $("#txtNombreLabelIndex").html("Hola, desconocido, ¿Cuál es tu nombre?");
+    } else {
+        $("#txtNombreLabelIndex").html("Hola amigo!");
+        $("#txtNombreIndex").val(name);
+    }
+}
